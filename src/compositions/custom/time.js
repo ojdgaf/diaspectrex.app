39f2e46668bef4fda27693d03933acabb5996c0c
Eugene Burdeinyi <ojdgaf@gmail.com>
2018-06-05 15:24:07 +0300

    time mixin

--- src/compositions/custom/time.js
+++ src/compositions/custom/time.js
@@ -1,7 +1,24 @@
 export default {
   install (Vue) {
-    window.unix = value => Vue.moment(value).unix()
+    const format = 'dddd, Do MMMM YYYY, HH:mm:ss'
     
-    window.iso = value => typeof value === 'number' ? Vue.moment.unix(value).toISOString() : Vue.moment(value).toISOString()
+    const now = () => Vue.moment().unix()
+    
+    const unix = (value = now()) =>
+      typeof value === 'number' ? value : Vue.moment(value).unix()
+    
+    const iso = (value = now()) =>
+      typeof value === 'number' ? Vue.moment.unix(value).toISOString() : Vue.moment(value).toISOString()
+    
+    const when = (value = now()) =>
+      typeof value === 'number' ? Vue.moment.unix(value).format(format) : Vue.moment(value).format(format)
+    
+    const fromNow = (value = now()) =>
+      typeof value === 'number' ? Vue.moment.unix(value).fromNow() : Vue.moment(value).fromNow()
+    
+    Object.defineProperty(Vue.prototype, '$unix', {value: unix})
+    Object.defineProperty(Vue.prototype, '$iso', {value: iso})
+    Object.defineProperty(Vue.prototype, '$when', {value: when})
+    Object.defineProperty(Vue.prototype, '$fromNow', {value: fromNow})
   }
 }
